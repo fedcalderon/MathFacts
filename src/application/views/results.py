2eb39c3228d859e3d4eb07c5cda83a1a5407cc0c
MiltonMcNeil <62513898+MiltonMcNeil@users.noreply.github.com>
2020-11-02 02:30:34 -0500

    Update results.py

--- src/application/views/results.py
+++ src/application/views/results.py
@@ -1,2 +1,30 @@
 # Showing all problems worked on by user
 # Milton
+def display_result(total, correct):
+    if total > 0:
+        result = correct / total
+        percentage = round((result * 100), 2)
+    if total == 0:
+        percentage = 0
+    print("You answered", total, "questions with", correct, "correct.")
+    print("Your score is ", percentage, "%. Thank you.", sep = "")
+
+
+def main():
+    display_intro()
+    display_menu()
+    display_separator()
+
+    option = get_user_input()
+    total = 0
+    correct = 0
+    while option != 5:
+        total = total + 1
+        correct = menu_option(option, correct)
+        option = get_user_input()
+
+    print("Finished Problems.")
+    display_separator()
+    display_result(total, correct)
+
+main()
