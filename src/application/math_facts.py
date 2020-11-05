d6698ecf8f7b31932efe570b47e88bf6c1e31935
aburrell1 <aburrell758@gmail.com>
2020-11-05 11:02:27 -0500

    Updates:     1. Program now runs from math_facts.py.     2. Save button now saves the user information to a csv file as well        as a dictionary.
    Signed-off-by: aburrell1 <aburrell758@gmail.com>

--- src/application/math_facts.py
+++ src/application/math_facts.py
@@ -1,4 +1,10 @@
 """
 This is the entry to the Math Facts Application
 """
+from src.application.views import registration
+
 print("Welcome to the Math Facts Application")
+
+if __name__ == '__main__':
+    app = registration.MyApplication()
+    app.mainloop()
