129983de09fb9cd9ac1795ae3a5389a87c1f5ed5
fedcalderon <fedcalderon@gmail.com>
2020-11-09 18:21:52 -0500

    Updated comments with instructions

--- src/application/views/welcome.py
+++ src/application/views/welcome.py
@@ -1,2 +1,11 @@
 # This is the welcome screen for all developers to collaborate
-# 
+# Need to decide on a common screen dimension for the program
+# Investigate if you have a master frame or screen, that changes content for each page
+# Program should have the ability to resize at will (stop at a minimum size), and full screen
+
+# 1: Micah: Design, a nice picture, colors, a logo
+# 2: Xade: Place the login and registration links, nice animation
+# 3: Andrew: Description of the program, graphics
+# 4: Milton: Link to terms of use (needs a new page)
+# 5: Asher: Test the connection between the login and registration pages, testing all the code
+# Team lead: Asher
