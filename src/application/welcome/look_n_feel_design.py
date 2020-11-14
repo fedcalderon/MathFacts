496066e00d5a960474603094f9831649302612c3
CamoN <micahblackman343@gmail.com>
2020-11-14 13:53:01 -0500

    Login Window

--- src/application/welcome/look_n_feel_design.py
+++ src/application/welcome/look_n_feel_design.py
@@ -1,1 +1,20 @@
-# Andrew
+# Micah
+from tkinter import *
+import os
+
+# Designing welcome screen
+
+def main_account_screen():
+    global main_screen
+    main_screen = Tk()
+    main_screen.geometry("1000x800")
+    main_screen.title("MathFacts")
+    main_screen.configure(background='yellow')
+    Label(text="Welcome to MathFacts!", bg="blue", width="300", height="2", font=("Calibri", 25)).pack()
+    Label(text="").pack()
+
+
+    main_screen.mainloop()
+
+
+main_account_screen()
