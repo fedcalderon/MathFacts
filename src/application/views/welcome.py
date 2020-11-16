47c25b31bc5741ff0ffed8d40d1d61802828ea38
fedcalderon <fedcalderon@users.noreply.github.com>
2020-11-16 18:06:40 -0500

    Decided to create directory and blank files for each of you to work on your own assignments and avoid conflicts.

--- src/application/views/welcome.py
+++ src/application/views/welcome.py
@@ -1,3 +1,6 @@
+#*******************
+#Nov 7, 2020
+#*******************
 # This is the welcome screen for all developers to collaborate
 # Need to decide on a common screen dimension for the program
 # Investigate if you have a master frame or screen, that changes content for each page
@@ -9,3 +12,19 @@
 # 4: Milton: Link to terms of use (needs a new page)
 # 5: Asher: Test the connection between the login and registration pages, testing all the code
 # Team lead: Asher
+
+#*******************
+#Nov 16, 2020
+#*******************
+# 1: Micah: Design, a nice picture, colors, a logo
+# 2: Xade: Place the login and registration links, nice animation. Collaborate with Asher in integrating all the code
+#          into welcome.py.
+# 3: Andrew: Description of the program, graphics. Collaborate with Asher in integrating all the code
+#            into welcome.py.
+# 4: Milton: Link to terms of use (needs a new page). Make it a pop up screen with generic terms
+#            of use (google it). Give it an OK button to exit. Collaborate with Asher in integrating all the code
+#            into welcome.py.
+# 5: Asher: In charge of integrating everyone else's code into welcome.py.
+#           Test the connection between the login and registration pages, testing all the code.
+# Team lead: Asher
+
