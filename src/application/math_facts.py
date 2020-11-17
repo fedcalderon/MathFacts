29d00123afee0c6748029b0f271131c6331b341f
aburrell1 <aburrell758@gmail.com>
2020-11-17 16:23:05 -0500

    Downloaded Andrew's code into my own branch and edited it a little. When math_facts.py is run, it opens welcome_description.py. Eventually, when the welcome screen frames are all put together, it will link to welcome.py with all the frame put into one view.
    Signed-off-by: aburrell1 <aburrell758@gmail.com>

--- src/application/math_facts.py
+++ src/application/math_facts.py
@@ -2,9 +2,14 @@
 This is the entry to the Math Facts Application
 """
 from src.application.views import registration
+from src.application.welcome import welcome_description
+import tkinter as tk
 
-print("Welcome to the Math Facts Application")
 
 if __name__ == '__main__':
-    app = registration.MyApplication()
-    app.mainloop()
+    root = tk.Tk()
+    root.title('MathFacts')
+    root.resizable(width=False, height=False)
+    root.geometry('500x300')
+    welcome_description.DescriptionFrame(root).pack(expand=True, fill='both')
+    root.mainloop()
