054f7f6ae6315ce91eb213cfc9bf7e94cc1dda4d
Xade-Hooper <62272846+Xade-Hooper@users.noreply.github.com>
2020-11-30 17:14:26 -0500

    Small Update on previous code.

--- src/application/welcome/welcome_page_links.py
+++ src/application/welcome/welcome_page_links.py
@@ -10,26 +10,33 @@ class Page_Links(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
 
+        self.Time_label = tk.StringVar()
+        self.Time_label.set("Time Left: #")
+
+        Time_label = ttk.Label(self, textvariable=self.Time_label,
+                               font=("TkDefaultFont", 10), wraplength=600)
+
         #Link buttons
 
         login_button = ttk.Button(self, text="Login", command= self.Login())
-        register_button = ttk.Button(self, text="Login", command= self.Register())
+        register_button = ttk.Button(self, text="Register", command= self.Register())
 
         #Grid
         login_button.grid(row=3, column=2, sticky=(tk.E+tk.W))
         register_button.grid(row=4, column=2, sticky=(tk.E+tk.W))
+        Time_label.grid(row=0, column=12, sticky=tk.E)
 
     def Login(self):
         if self.Login().get:
             os.system(login.py)
         else:
-            print('')
+            exit()
 
     def Register(self):
         if self.register().get:
             os.system(registration.py)
         else:
-            print('')
+            exit()
 
 #if __name__ == '__main__':
 #    root = tk.Tk()
