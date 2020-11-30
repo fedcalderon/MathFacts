24bb5cd42e2bbe996c300b1459c577b05ff41eb4
Xade-Hooper <62272846+Xade-Hooper@users.noreply.github.com>
2020-11-30 02:29:59 -0500

    Base Code commit: Page link code(incomplete)
    work completed/ in progress:
    1. base code for page links
    2. button placements
    work uncompleted:
    1. animation
    2. page test for buttons
    Errors:
    1. Page doesnt show up instead it skips straight to calling the login page link
    Information needed:
    1. How to create an animation for the action of switching pages

--- src/application/welcome/welcome_page_links.py
+++ src/application/welcome/welcome_page_links.py
@@ -1,1 +1,56 @@
 # Xade
+
+import tkinter as tk
+import tkinter as ttk
+import  os
+from src.application.views import login
+from src.application.views import registration
+
+class Page_Links(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+
+        #Link buttons
+
+        login_button = ttk.Button(self, text="Login", command= self.Login())
+        register_button = ttk.Button(self, text="Login", command= self.Register())
+
+        #Grid
+        login_button.grid(row=3, column=2, sticky=(tk.E+tk.W))
+        register_button.grid(row=4, column=2, sticky=(tk.E+tk.W))
+
+    def Login(self):
+        if self.Login().get:
+            os.system(login.py)
+        else:
+            print('')
+
+    def Register(self):
+        if self.register().get:
+            os.system(registration.py)
+        else:
+            print('')
+
+#if __name__ == '__main__':
+#    root = tk.Tk()
+#    root.title('Math Facts Practice')
+#    root.resizable(width=False, height=False)
+#    root.geometry('500x300')
+#   Page_Links(root).pack(expand=True, fill='both')
+#    root.mainloop()
+
+
+    class test_screen_Settings(tk.Tk):
+        """Screen settings"""
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.geometry("600x500")
+            self.resizable(width=False, height=False)
+            Page_Links(self).grid(sticky=(tk.E + tk.W + tk.N + tk.S))
+            self.columnconfigure(0, weight=1)
+
+
+    if __name__ == '__main__':
+        app = test_screen_Settings()
+        app.mainloop()
