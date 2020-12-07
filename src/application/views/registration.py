060692d7ee57b0db286f0393314a6d9ce1334a7b
aburrell1 <46632750+aburrell1@users.noreply.github.com>
2020-12-07 08:54:23 -0500

    Moved the json file to a new file called views_data. Other data files required by different views can go in there.
    Signed-off-by: aburrell1 <aburrell758@gmail.com>

--- src/application/views/registration.py
+++ src/application/views/registration.py
@@ -5,6 +5,7 @@
 import tkinter as tk
 from tkinter import ttk
 import json
+import src.application
 import os
 
 
@@ -134,8 +135,6 @@ class MyApplication(tk.Tk):
         self.user_count = 0
         self.users_list = []
 
-        with open('users.json', 'w') as jsonfile:
-            json.dump({f"user {self.user_count}": {}}, jsonfile)
 
     def save(self):
         self.user_count = self.user_count
@@ -176,12 +175,13 @@ class MyApplication(tk.Tk):
                                                   "                   ", font=("TkDefaultFont", 10), wraplength=600)
                 self.field.grid(row=1400, column=0, sticky=tk.W)
 
-                # with open(r'C:\Users\aburrell\PycharmProjects\MathFacts\src\application\users.json', 'w') as jsonfile:
+                # with open(r'users.json', 'w') as jsonfile:
                 #     json.dump({f"user {self.user_count}": all_information}, jsonfile)
 
+                with open(r'views_data\users.json', 'w') as jsonfile:
+                    json.dump({f"user {self.user_count}": all_information}, jsonfile)
 
-
-                with open('users.json') as jsonfile:
+                with open(r'views_data\users.json') as jsonfile:
                     users_data = json.load(jsonfile)
 
                 self.user_count = self.user_count + 1
@@ -192,7 +192,7 @@ class MyApplication(tk.Tk):
                     for key in user:
                         all_users.update({key : user[key]})
 
-                with open('users.json', 'w') as jsonfile:
+                with open(r'views_data\users.json', 'w') as jsonfile:
                     json.dump(all_users, jsonfile)
 
 
