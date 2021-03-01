c2b67258c10b2d54d5ac21d572b9559c1bed3c10
aburrell1 <46632750+aburrell1@users.noreply.github.com>
2021-03-01 17:21:05 -0500

    added the terms of use and login windows to all_screens_reworked
    Signed-off-by: aburrell1 <aburrell758@gmail.com>

--- src/application/tests/all_screens_reworked.py
+++ src/application/tests/all_screens_reworked.py
@@ -36,10 +36,10 @@ class MyApplication(tk.Tk):
                                               command=lambda: self.change_screen(
                                                   self.welcome_screen, self.registration_screen))
 
-        # self.terms_of_use_button = ttk.Button(self, text="Terms Of Use", command=lambda: self.change_screen(
-        #     self.welcome_screen, self.terms_of_use_screen))
+        self.terms_of_use_button = ttk.Button(self, text="Terms Of Use", command=lambda: self.change_screen(
+            self.welcome_screen, self.terms_of_use_screen))
 
-        self.welcome_screen.extend([self.registration_button])
+        self.welcome_screen.extend([self.registration_button, self.terms_of_use_button])
 
         # Append all frames to the welcome view
         for item in self.welcome_screen:
@@ -57,9 +57,9 @@ class MyApplication(tk.Tk):
         self.desc_label = ttk.Label(
             self, text=self.terms_of_use_description, wraplength=400, font=("TkDefaultFont", 11))
 
-        # self.terms_of_use_screen = [ttk.Label(self, text="Terms Of Use",
-        #                                       font=("TkDefaultFont", 27), wraplength=600), self.desc_label]
-        #
+        self.terms_of_use_screen = [ttk.Label(self, text="Terms Of Use",
+                                              font=("TkDefaultFont", 27), wraplength=600), self.desc_label]
+
         # self.terms_of_use_back = tk.Button(self, text="Back",
         #                                       command=lambda: self.change_screen(
         #                                           self.terms_of_use_screen, [welcome.IconFrame(self),
