5740d0afd6b283c9a919141119e22cec91779dc4
aburrell1 <aburrell758@gmail.com>
2021-04-21 09:43:30 -0400

    Added the logger to results.py to log grades and task completion date.
    Signed-off-by: aburrell1 <aburrell758@gmail.com>

--- src/application/new_views/results.py
+++ src/application/new_views/results.py
@@ -4,6 +4,7 @@ from tkinter import *
 from pathlib import Path
 import json
 from datetime import datetime
+import src.application.tests.modified_logger as logger
 
 
 def save_results(questions_list, student_id):
@@ -34,6 +35,7 @@ class LinksFrame(Frame):
     def __init__(self, parent, problems, student_id, **kwargs):
         super().__init__(parent, **kwargs)
         self.problems = problems
+        self.score = 0
         Label(self, text='GRADES:', wraplength=400, font=("TkDefaultFont", 20)).grid(sticky=W)
 
         # List and print all problem set grades.
@@ -69,6 +71,10 @@ class LinksFrame(Frame):
             # Give the score
             self.score = round((self.correct_answers / self.total_questions) * 100, 1)
             self.score_text = f"Assignment grade is {self.score:g}%"
+            self.logger = logger.Logger('user_grades.log',
+                                        f"Completed task {self.problems.questions.ID} on {datetime.now()}. "
+                                        f"Score is {self.score:g}\n")
+
             Label(self, text=self.score_text, wraplength=400, font=("TkDefaultFont", 11)).grid(sticky=W)
 
             save_results(self.problems.questions_list, student_id)
@@ -78,7 +84,7 @@ class LinksFrame(Frame):
                   wraplength=400, font=("TkDefaultFont", 11)).grid(sticky=W)
 
         Button(self, text="Start a new exercise", command=lambda: parent.change_screen(
-            [self], parent.problem_selection_screen)).grid()
+        [self], parent.problem_selection_screen)).grid()
 
 
 # Results screen
