567e55ae93ac5f61226260151223f143f0a989db
MiltonMcNeil <62513898+MiltonMcNeil@users.noreply.github.com>
2021-04-24 19:33:27 -0400

    Create dashboard.py

---
+++ src/application/new_views/dashboard.py
@@ -0,0 +1,9 @@
+from tkinter import *
+top=Tk()
+top.title("Dashboard")
+top.geometry("300x300")
+lbl=Label(top,text="Dashboard", font=("", 25))
+lbl.place(x=80,y=100)
+btn=Button(top, text="Exit", font=("", 15))
+btn.place(x=35,y=20)
+top.mainloop()
