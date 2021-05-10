a969b1ce4689257b2bd095ac3b93785a8f904b7a
aburrell1 <aburrell758@gmail.com>
2021-05-10 17:59:51 -0400

    Fixed dashboard
    Signed-off-by: aburrell1 <aburrell758@gmail.com>

--- src/application/new_views/dashboard.py
+++ src/application/new_views/dashboard.py
@@ -5,7 +5,6 @@ class Dashboard(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent, pady=20)
         # self.configure(bg="gold")
-        self.lbl=tk.Label(parent,text="Dashboard", font=("", 25))
-        self.lbl.place(x=80,y=100)
+        tk.Label(parent, text="Dashboard", font=("", 25))
+        # self.lbl.grid()
         self.btn=tk.Button(parent, text="Exit", font=("", 15))
-        self.btn.place(x=35,y=20)
