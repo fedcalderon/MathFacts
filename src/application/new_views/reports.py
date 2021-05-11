3a729c97b3e20d906669fca554eec615c0753f2c
Andrew <anthom03@gmail.com>
2021-05-11 17:05:48 -0400

    Fixed the reports screen

--- src/application/new_views/reports.py
+++ src/application/new_views/reports.py
@@ -1,7 +1,5 @@
 import tkinter as tk
 from tkinter import ttk
-import json
-from pathlib import Path
 from src.application.new_views import stats_graph as stats_graph
 
 """This view is a work in progress. When it is completed, it will store all the lesson results in a graph. """
@@ -12,31 +10,29 @@ class ReportsFrame(tk.Frame):
         super().__init__(parent, **kwargs)
 
         self.report_graph = stats_graph.ReportsGraph()
-        self.icon = IconFrame(parent)
+        self.graph = None
 
-        self.Main_Label = ttk.Label(self, text="User Reports Screen:", font=("TkDefaultFont", 27), wraplength=600)
-        self.Main_Label.grid(row=0, column=0, sticky=tk.W)
+        self.Main_Label = ttk.Label(self, text="User Reports", font=("TkDefaultFont", 27))
+        self.Main_Label.grid(row=0, column=0)
 
-        self.Save = ttk.Button(self, text="Click to show Reports Graph",
-                               command=lambda: self.report_graph.display_graph(parent))
-
-        self.Save = ttk.Button(self, text="Click to show Reports Graph",
-                               command=lambda: self.show_icon(parent))
-
-        self.Save.grid(row=1200, column=0)
+        self.Save = ttk.Button(self, text="Update Reports Graph", command=lambda: self.show_icon(parent))
+        self.Save.grid(row=1, column=0)
 
     def show_icon(self, parent):
         if parent.current_screen == parent.reports_screen:
-            # self.icon.grid()
-            # self.icon.image_label.grid()
-            parent.reports_screen.extend([self.icon, self.icon.image_label])
-            for item in parent.reports_screen:
-                item.grid()
+            self.report_graph.generate_graph(parent)
+            self.graph = GraphFrame(self)
+            self.graph.grid(row=2, column=0)
+            # self.graph.image_label.grid()
+            # parent.reports_screen.extend([self.graph, self.graph.image_label])
+            # for item in parent.reports_screen:
+            #     item.grid()
 
 
-class IconFrame(tk.Frame):
-    """Contains and displays the description of the Math Facts Practice application."""
+class GraphFrame(tk.Frame):
+    """Contains and displays a graph of the student's quiz grades."""
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
-        self.image = tk.PhotoImage(file="view_images/results_graph.png")
+        self.image = tk.PhotoImage(file=stats_graph.graph_path)
         self.image_label = tk.Label(self, image=self.image)
+        self.image_label.pack()
